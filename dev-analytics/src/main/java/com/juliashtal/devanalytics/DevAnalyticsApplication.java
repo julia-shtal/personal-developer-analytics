4a5af8331f7e18f312da424e4dce22e467b3ed7f
Julia Shtal <shtaljulia@gmail.com>
2026-02-02 18:41:02 +0100

    PDA-5 Added: settings.

---
+++ dev-analytics/src/main/java/com/juliashtal/devanalytics/DevAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.juliashtal.devanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevAnalyticsApplication.class, args);
+    }
+
+}
